7942991f80cfa3cbdb04498bbf469735258c4f9a
Starscout <polleysjonathan@gmail.com>
2024-06-01 13:30:06 -0600

    Added JavaScript to blog

--- blog/blog.js
+++ blog/blog.js
@@ -23,5 +23,61 @@ const articles = [
 		ages: '12-16',
 		genre: 'Fantasy',
 		stars: '⭐⭐⭐⭐'
+	},
+	{
+		id: 3,
+		title: "Belgariad Book One: Pawn of Prophecy",
+		date: "Feb 12, 2022",
+		description:
+			"A fierce dispute among the Gods and the theft of a powerful Orb leaves the World divided into five kingdoms. Young Garion, with his \"Aunt Pol\" and an elderly man calling himself Wolf --a father and daughter granted near-immortality by one of the Gods -- set out on a complex mission.",
+		imgSrc:
+			"https://images-na.ssl-images-amazon.com/images/I/41ZxXA+nInL.jpg",
+		imgAlt: "Book cover for Pawn of Prophecy",
+		ages: "12-16",
+		genre: "Fantasy",
+		stars: "⭐⭐⭐⭐⭐"
 	}
 ]
+articles.forEach((article)=>{
+	const id = article.id;
+	const title = article.title;
+	const date = article.date;
+	const desc = article.description;
+	const image = article.imgSrc;
+	const imgDesc = article.imgAlt;
+	const ages = article.ages;
+	const genre = article.genre;
+	const stars = article.stars;
+
+  const section = document.createElement('section');
+  section.className = 'book_id';
+  section.id = id;
+
+  const leftSection = document.createElement('section');
+  leftSection.className = 'left';
+  leftSection.innerHTML = `
+	  <h3 id="date">${date}</h3>
+	  <h4 id="age_range">${ages}</h4>
+	  <h4 id="genre">${genre}</h4>
+	  <h4 id="rating">${stars}</h4>
+  `;
+
+  const mainSection = document.createElement('section');
+  mainSection.className = 'main';
+  mainSection.innerHTML = `
+	  <h2 id="title">${title}</h2>
+	  <img id="book_image" src="${image}" alt="${imgDesc}">
+	  <p id="description">${desc}</p>
+  `;
+
+  const rightSection = document.createElement('section');
+  rightSection.className = 'right';
+  rightSection.innerHTML = `<p>Filters will go here</p>`;
+
+  section.appendChild(leftSection);
+  section.appendChild(mainSection);
+  section.appendChild(rightSection);
+
+  document.getElementById('book-container').appendChild(section);
+
+});
